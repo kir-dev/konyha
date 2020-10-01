bc49cbc006e82c4c8d0c9f7a81a058db0304c600
poiuztrewq1 <bucsy.beni@gmail.com>
2020-10-01 19:11:15 +0200

    Added ruby magic and formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Creating kitchens 3 to 18'
-16.times do |t|
-  Kitchen.create(floor: t+3 )
+(3..18).each do |t|
+  Kitchen.create(floor: t)
 end
 
 
@@ -19,16 +18,16 @@ min_comment_per_appliance = 3
 max_comment_per_appliance = 20
 
 Kitchen.all.each do |kitchen|
-  Faker::Number.between(from: min_appliance_per_kitchen , to: max_appliance_per_kitchen).times do |num|
-    appliance = Appliance.create(name: "#{Faker::Appliance.brand} #{Faker::Appliance.equipment}",
-                                 category: Appliance.categories.values.sample,
-                                 status: Appliance.statuses.values.sample,
-                                 description: Faker::Lorem.sentence,
-                                 kitchen: kitchen)
+  Faker::Number.between(from: min_appliance_per_kitchen, to: max_appliance_per_kitchen).times do |num|
+    appliance   = Appliance.create(name:        "#{Faker::Appliance.brand} #{Faker::Appliance.equipment}",
+                                   category:    Appliance.categories.values.sample,
+                                   status:      Appliance.statuses.values.sample,
+                                   description: Faker::Lorem.sentence,
+                                   kitchen:     kitchen)
     num_comment = Faker::Number.between(from: min_comment_per_appliance, to: max_comment_per_appliance)
     num_comment.times do |t|
-      ApplianceComment.create(body: Faker::Quote.famous_last_words,
-                              category: ApplianceComment.categories.values.sample,
+      ApplianceComment.create(body:      Faker::Quote.famous_last_words,
+                              category:  ApplianceComment.categories.values.sample,
                               appliance: appliance)
     end
     puts "  Added #{num_comment} comment to appliance: #{appliance.name} at floor #{kitchen.floor}"
