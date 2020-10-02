cf6f75989a660655888c3c87f36577b68efa18a1
Berci <bercike445@gmail.com>
2020-10-02 13:29:49 +0200

    Added language selector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_languages
+
+  private
+    def set_languages
+      @languages = I18n.available_locales
+      print("languages:", @languages)
+    end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
