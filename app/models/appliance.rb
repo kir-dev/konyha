6188e20ef65fbf5e41a925d9758302774d310610
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-10-03 20:05:30 +0200

    Link Appliance status to commnents  (#23)

--- app/models/appliance.rb
+++ app/models/appliance.rb
@@ -8,4 +8,16 @@ class Appliance < ApplicationRecord
   enum status: %i[operational broken]
 
   include ApplianceStateMachine
+
+  def self.actions
+    Appliance.aasm.events.map(&:name)
+  end
+
+  def valid_actions
+    Appliance.actions.select { |e| send("may_#{e}?") }
+  end
+
+  def valid_action?(transition)
+    valid_actions.include? transition
+  end
 end
--- app/models/appliance_comment.rb
+++ app/models/appliance_comment.rb
@@ -1,8 +1,25 @@
 class ApplianceComment < ApplicationRecord
   belongs_to :appliance
 
-  enum category: %i[fix note problem]
+  enum category: %i[fix note break]
 
-  validates :body, length: {maximum: 500}
-  validates :category, inclusion: {in: ApplianceComment.categories}
+  validates :category, presence: true
+
+  before_save :update_appliance_status
+
+  def available_actions
+    appliance.valid_actions.push :note
+  end
+
+  private
+
+  def update_appliance_status
+    begin
+      appliance.send((category + '!').to_sym) if appliance.valid_action?(category.to_sym)
+    rescue AASM::InvalidTransition
+      errors.add(:category, :invalid)
+
+      raise ActiveRecord::RecordInvalid
+    end
+  end
 end
