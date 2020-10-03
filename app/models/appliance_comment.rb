58fb95c6600836facd9821adfd2ff53208e41cc4
poiuztrewq1 <bucsy.beni@gmail.com>
2020-10-03 19:46:08 +0200

    Appliance comment validation with rails magic

--- app/models/appliance_comment.rb
+++ app/models/appliance_comment.rb
@@ -2,4 +2,7 @@ class ApplianceComment < ApplicationRecord
   belongs_to :appliance
 
   enum category: %i[fix note problem]
+
+  validates :body, length: {maximum: 500}
+  validates :category, inclusion: {in: ApplianceComment.categories}
 end
