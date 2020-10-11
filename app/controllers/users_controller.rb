192a995d77be0259e1bef479fb15e434a2f7a3ab
Sepsi László <48379676+SepsiLaszlo@users.noreply.github.com>
2020-10-11 10:48:53 -0700

    update project for staging (#43)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:uid, :email, :name, :floor)
+      params.require(:user).permit( :email, :name, :floor)
     end
 end
